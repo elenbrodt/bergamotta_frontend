abafe4b32733548123c7d49b08ba62a11a880c07
Victor Ledur <victorlledur@gmail.com>
2023-01-05 14:51:06 -0300

    update: index, styles about + images

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,18 +1,54 @@
-import Box from "../../components/Box";
 import Header from "../../components/Header";
-import { Title } from './styles';
+import { Title, Title2, Text, Overlaid1, Orangebg1, Imgbody, UpperBody, UpperBodyLeft, LinkButton, BottonBody, BottonBodyLeft, BottonBodyRight } from './styles';
+import bodyimg1 from "../../assets/imagesAbout/veg 001 1.png"
+import vector1 from "../../assets/imagesAbout/Vector1.png"
+import vector2 from "../../assets/imagesAbout/Vector2.png"
+import bodyimg2 from "../../assets/imagesAbout/Rectangle 62.png"
+import bodyimage3 from "../../assets/imagesAbout/persona 12 1.png"
+// import Link from "../../components/Link";
+
+
 
 
 function About() {
     return (
-        <div>
-            <Header />
-            <Box background="green">
-                <p>test</p>
-            </Box>
-            <Title>Te ajudamos a encontrar<br></br>
-            experiencias gastronômicas incríveis</Title>
-        </div>
+        <section>
+            <div>
+                <Header />                
+            </div>
+            <UpperBody>
+                <UpperBodyLeft>
+                    <Title>Te ajudamos a encontrar<br></br>
+                        experiencias gastronômicas incríveis</Title>
+                    <LinkButton texto="Buscar experiências" redirect="" />
+                </UpperBodyLeft>
+                <div>
+                    <Imgbody src={bodyimg1} alt="Experiências Incríveis" />
+                </div>
+            </UpperBody>
+            <BottonBody>
+                <BottonBodyLeft>
+                    <Orangebg1 src={vector1} alt="Fundo Laranja" />
+                    <Overlaid1 src={bodyimg2} alt="Casal Empolgado" />
+                </BottonBodyLeft>
+                <BottonBodyRight>
+                    <Title2>Quem Somos</Title2>
+                    <Text>A Bergamotta é uma plataforma que contecta<br></br>
+                        restaurantes a clientes que adotaram uma dieta<br></br>
+                        vegetariana ou vegana sem abrir mão de vivenciar, da<br></br>
+                        sua maneira, grandes momentos gastronômicos.</Text>
+                    <Title2>Nosso Objetivo</Title2>
+                    <Text>Te auxiliar na busca de restaurantes, bares e afins,<br></br>
+                        gerando uma experiência completa em um local livre de<br></br>
+                        alimentos de origem animal, proporcionando um<br></br>
+                        momento gastronômico do jeitinho que você procura.</Text>
+                </BottonBodyRight>
+            </BottonBody>
+            <div>
+                <img src={vector2} alt="Fundo Laranja" />
+                <img src={bodyimage3} alt="Senhor Impressionado" />
+            </div>
+        </section>
     );
 }
 
--- src/pages/about/styles.ts
+++ src/pages/about/styles.ts
@@ -1,8 +1,77 @@
-import {colors} from "../../styles/theme";
+import { colors } from "../../styles/theme";
 import styled from 'styled-components';
+import Link from "../../components/Link";
 
 
 export const Title = styled.h1`
-    margin: 0;
-    color: ${colors.black}
+margin: 0;
+width: 740px;
+height: 170px;
+font-weight: 700;
+font-size: 40px;
+line-height: 55px;
+text-align: center;
+z-index:1;
+color: ${colors.black}
+`
+export const Title2 = styled.h2`
+margin:0;
+width: 322px;
+height: 97.45px;
+font-weight: 700;
+font-size: 40px;
+color: ${colors.error}
+`
+export const Text = styled.text`
+margin:0;
+color: ${colors.black};
+width: 504px;
+height: 125.58px;
+font-weight: 400;
+font-size: 20px;
+line-height: 28px;
+`
+export const Imgbody = styled.img` 
+margin:0;
+width: 706.03px;
+height: 483px;
+`
+
+export const Orangebg1 = styled.img` 
+margin:0;
+width: 898.4px;
+height: 1039.92px;
+`
+export const Overlaid1 = styled.img`
+position: absolute;
+margin-top: 300px;
+z-index: 1;
+width: 725px;
+height: 743px;
+`
+export const UpperBody = styled.div` 
+display: flex;
+justify-content: center;
+`
+export const UpperBodyLeft = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center
+`
+export const BottonBody = styled.div`
+display: flex;
+justify-content: space-between;
+`
+export const BottonBodyLeft = styled.div`
+display: flex;
+position: relative;
+`
+export const BottonBodyRight = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+`
+
+export const LinkButton = styled(Link)`
+margin:0
 `
