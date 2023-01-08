d0ebfd97aae8598226932a0c1da2a25ada542a2c
Victor Ledur <victorlledur@gmail.com>
2023-01-08 00:47:54 -0300

    update: Routes

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -12,7 +12,7 @@ export default function Routes() {
     <BrowserRouter>
         <WrapperRoutes>
             <Route path="/" element={<Search/>}/>
-            <Route path="/restaurantes" element={<Home/>}/>
+            <Route path="/home" element={<Home/>}/>
              <Route path="/sobrenos" element={<About/>}/>    
             <Route path="/blog" element={<Blog/>}/>
             <Route path="/login" element={<Login/>}/>
