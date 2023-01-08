613b97b67b2038c454edc2d318dad4e7e2012df8
Victor Ledur <victorlledur@gmail.com>
2023-01-08 02:35:30 -0300

    feat: all home files

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,21 +1,99 @@
-import Header from "../../components/Header"
-import Box from "../../components/Box"
-import Link from "../../components/Button"
-import Veg from "../../assets/imagesHome/veg.png";
-import { P, VegImg, ConnectionDiv, ConnectionBlock } from "./style";
+import Header from "../../components/Header";
+import {
+    Title, Title2, TitleButton, Text, Overlaid1, Orangebg1, Imgbody,
+    UpperBody, UpperBodyLeft, UpperBodyRight, MidBody, MidBodyLeft, MidBodyRight, LowerBody, LowerBodyLeft, LowerBodyRight, Title3, ListStyle, ListItens, Orangebg2, Overlaid2,
+    GreenBannerContainer, FormContainer, FormDiv, Title4, Labels, NormalInput, TextInput,
+    FormBtn, FormContainerRight, Orangebg3, Overlaid3
+} from "./styles";
+import bodyimg0 from "../../assets/imagesAbout/veg 001 1.png"
+import vector1 from "../../assets/imagesAbout/Vector1.png"
+import vector2 from "../../assets/imagesAbout/Vector2.png"
+import bodyimg1 from "../../assets/imagesAbout/Rectangle 62.png"
+import bodyimage2 from "../../assets/imagesAbout/persona 12 1.png"
+import vector3 from "../../assets/imagesAbout/Vector3.png"
+import bodyimg3 from "../../assets/imagesAbout/persona 6 1.png"
+import GreenBanner from "../../components/GreenBanner";
 
 function Home() {
     return (
-        <div>
-            <Header />
-            <Box background="white">
-                <ConnectionDiv>
-                    <ConnectionBlock>
-                        <P>Conectando pessoas, propósitos e experiências gastronômicas</P>
-                    </ConnectionBlock>
-                    {/* <VegImg src={Veg} alt="comida vegana" /> */}
-                </ConnectionDiv>
-            </Box>
+        <div className="App">
+            <header>
+                <Header />
+            </header>
+            <body>
+                <UpperBody>
+                    <UpperBodyLeft>
+                        <Title>Te ajudamos a encontrar<br></br>
+                            experiencias gastronômicas incríveis</Title>
+                        <TitleButton href="/">Buscar experiências</TitleButton>
+                    </UpperBodyLeft>
+                    <UpperBodyRight>
+                        <Imgbody src={bodyimg0} alt="Experiências Incríveis" />
+                    </UpperBodyRight>
+                </UpperBody>
+                <MidBody>
+                    <MidBodyLeft>
+                        <Orangebg1 src={vector1} alt="Fundo Laranja" />
+                        <Overlaid1 src={bodyimg1} alt="Casal Empolgado" />
+                    </MidBodyLeft>
+                    <MidBodyRight>
+                        <Title2>Quem Somos</Title2>
+                        <Text>A Bergamotta é uma plataforma que contecta<br></br>
+                            restaurantes a clientes que adotaram uma dieta<br></br>
+                            vegetariana ou vegana sem abrir mão de vivenciar, da<br></br>
+                            sua maneira, grandes momentos gastronômicos.</Text>
+                        <Title2>Nosso Objetivo</Title2>
+                        <Text>Te auxiliar na busca de restaurantes, bares e afins,<br></br>
+                            gerando uma experiência completa em um local livre de<br></br>
+                            alimentos de origem animal, proporcionando um<br></br>
+                            momento gastronômico do jeitinho que você procura.</Text>
+                    </MidBodyRight>
+                </MidBody>
+                <LowerBody>
+                    <LowerBodyLeft>
+                        <Title3>Por que se tornar um parceiro?</Title3>
+                        <ListStyle>
+                            <ListItens>Reforce sua presença digital</ListItens>
+                            <ListItens>Atraia novos públicos</ListItens>
+                            <ListItens>Obtenha feedbacks</ListItens>
+                            <ListItens>Fortaleça sua reputação</ListItens>
+                            <ListItens>Fidelize seus clientes</ListItens>
+                        </ListStyle>
+                    </LowerBodyLeft>
+                    <LowerBodyRight>
+                        <Orangebg2 src={vector3} alt="Fundo Laranja" />
+                        <Overlaid2 src={bodyimg3} alt="Mulher apontando" />
+                    </LowerBodyRight>
+                </LowerBody>
+                <GreenBannerContainer>
+                    <GreenBanner texto="Cadastre-se agora mesmo e tenha acesso a milhares de restaurantes." />
+                </GreenBannerContainer>
+                <FormContainer>
+                    <div>
+                        <Title4>Dúvidas? Fale com a gente</Title4>
+                        <FormDiv>
+                            <br />
+                            <Labels>Nome</Labels>
+                            <br />
+                            <NormalInput type="name" name="name" />
+                            <br />
+                            <Labels>Whatsapp</Labels>
+                            <br />
+                            <NormalInput type="whatsapp" name="whatsapp" />
+                            <br />
+                            <Labels>Mensagem</Labels>
+                            <br />
+                            <TextInput></TextInput>
+                            <br />
+                            <FormBtn href="/">Enviar</FormBtn>
+                        </FormDiv>
+                    </div>
+                    <FormContainerRight>
+                        <Orangebg3 src={vector2} alt="Fundo Laranja" />
+                        <Overlaid3 src={bodyimage2} alt="Senhor Impressionado" />
+                    </FormContainerRight>
+                </FormContainer>
+            </body>
         </div>
     );
 }
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,290 @@
+import { colors, fonts } from "../../styles/theme";
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+margin: 0;
+width: 740px;
+height: 170px;
+font-weight: 700;
+font-size: 40px;
+line-height: 55px;
+text-align: center;
+z-index:1;
+color: ${colors.black};
+font-family: ${fonts.primary};
+margin-left: 27px;
+@media (max-width:475px){
+        margin-top: 30px;
+    }
+`
+export const Title2 = styled.h2`
+margin:0;
+width: 322px;
+height: 97.45px;
+font-weight: 700;
+font-size: 40px;
+color: ${colors.primary};
+font-family: ${fonts.primary};
+`
+export const TitleButton = styled.a`
+display: flex;
+justify-content: center;
+text-align: center;
+align-items: center;
+margin-top: 10px;
+margin: 0;
+text-decoration: none;
+width: 284px;
+height: 60px;
+background: ${colors.secondary};
+border-radius: 36px;
+border: hidden;
+font-family: ${fonts.secondary};
+font-weight: 700;
+font-size: 20px;
+line-height: 28px;
+font-weight: 700;
+font-size: 20px;
+line-height: 28px;
+color: ${colors.white};
+`
+export const Text = styled.text`
+margin:0;
+color: ${colors.black};
+width: 504px;
+height: 125.58px;
+font-weight: 400;
+font-size: 20px;
+line-height: 28px;
+font-family: ${fonts.secondary};
+`
+export const Imgbody = styled.img` 
+margin:0;
+width: 706.03px;
+height: 483px;
+`
+export const Orangebg1 = styled.img` 
+margin-top:-220px;
+margin-bottom: -220px;
+width: 808.4px;
+height: 1039.92px;
+@media (max-width:475px){
+        width:508px;
+        height: 739px;
+    }
+`
+export const Overlaid1 = styled.img`
+position: absolute;
+margin-top: 80px;
+z-index: 1;
+width: 725px;
+height: 743px;
+float: inline-start;
+@media (max-width:475px){
+        width: 425px;
+        height: 443px;
+    }
+`
+export const UpperBody = styled.div` 
+width: 100%;
+display: flex;
+justify-content: space-between;
+margin-bottom: 125px;
+flex-wrap: wrap-reverse;
+`
+export const UpperBodyLeft = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+gap: 15px;
+`
+export const UpperBodyRight = styled.div`
+@media (max-width:475px){
+        display: none;
+    }
+`
+export const MidBody = styled.div`
+display: flex;
+justify-content: space-between;
+background-color: ${colors.light_one};
+flex-wrap: wrap;
+@media (max-width:1327px){
+        background-color: ${colors.white};
+        align-content: center;
+    }
+`
+export const MidBodyLeft = styled.div`
+display: flex;
+position: sticky;
+background-color: ${colors.light_one};
+z-index: 1;
+margin-top: 100px;
+margin-bottom: 100px;
+@media (max-width:475px){
+        justify-content: center;
+    }
+`
+export const MidBodyRight = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+background-color: ${colors.light_one};
+margin-top: 100px;
+margin-bottom: 100px;
+background-color: ${colors.light_one};
+@media (max-width:1327px){
+        margin-top: 150px;
+        background-color: ${colors.white};
+        align-items: center;
+        padding-left: 35%;
+    }
+`
+
+export const LowerBody = styled.div`
+margin-top: 122px;
+display: flex;
+flex-wrap: wrap;
+justify-content: space-between;
+`
+export const LowerBodyLeft = styled.div`
+display: flex;
+flex-direction: column;
+align-items: flex-start;
+padding-left: 4% ;
+justify-content: center;
+`
+export const LowerBodyRight = styled.div`
+display: flex;
+position: relative;
+`
+export const Title3 = styled.h2`
+width: 600px;
+height: 44px;
+font-weight: 700;
+font-size: 40px;
+line-height: 44px;
+font-family: ${fonts.primary};
+color: ${colors.primary}
+`
+export const ListStyle = styled.ul`
+list-style-image: url(https://i.postimg.cc/7PGp7BQV/check.png);
+`
+export const ListItens = styled.li`
+font-family: ${fonts.secondary};
+color: ${colors.black};
+font-weight: 400;
+font-size: 20px;
+line-height: 60px;
+`
+export const Orangebg2 = styled.img`
+margin-bottom: 5px;
+width: 706px;
+height: 825.25px;
+`
+export const Overlaid2 = styled.img`
+position: absolute;
+margin-right: 160px;
+margin-top: 84px;
+z-index: 1;
+width: 706px;
+height: 740px;
+`
+
+export const GreenBannerContainer = styled.div`
+margin-top: -7px;
+width: 100%;
+`
+export const FormContainer = styled.div`
+display: flex;
+justify-content: space-between;
+margin-top: 12px;
+flex-wrap: wrap-reverse;
+`
+export const FormContainerRight = styled.div`
+display: flex;
+position: relative;
+`
+export const FormDiv = styled.form`
+display: flex;
+flex-direction: column;
+margin-left: 57px;
+`
+export const Title4 = styled.h3`
+width: 417px;
+height: 36px;
+margin-left: 57px;
+font-weight: 700;
+font-size: 32px;
+line-height: 35px;
+color: ${colors.primary};
+font-family: ${fonts.primary};
+`
+
+export const NormalInput = styled.input`
+width: 527px;
+height: 40px;
+background: rgba(255, 255, 255, 0.08);
+border: 1px solid #B6BFD8;
+border-radius: 10px;
+`
+
+export const TextInput = styled.textarea`
+width: 527px;
+height: 120px;
+box-sizing: border-box;
+background: rgba(255, 255, 255, 0.08);
+border: 1px solid #B6BFD8;
+border-radius: 8px;
+`
+
+export const Labels = styled.label`
+font-weight: 700;
+font-size: 20px;
+line-height: 28px;
+color: ${colors.dark_one};
+font-family: ${fonts.secondary};
+`
+
+export const FormBtn = styled.a`
+text-decoration: none;
+display: flex;
+justify-content: center;
+text-align: center;
+align-items: center;
+margin: 0;
+width: 158px;
+height: 60px;
+padding: 0 3%;
+border-radius: 36px;
+background: ${colors.secondary};
+border: hidden;
+align-self: flex-end;
+font-family: ${fonts.secondary};
+font-weight: 700;
+font-size: 20px;
+line-height: 28px;
+color: ${colors.white}
+`
+
+export const Orangebg3 = styled.img`
+width: 780.88px;
+height: 662.04px;
+@media (max-width:475px){
+        width: 480px;
+        height: 362px;
+    }
+`
+export const Overlaid3 = styled.img`
+position: absolute;
+margin-top: 160px;
+margin-right: 160px;
+z-index: 1;
+width: 685px;
+height: 501px;
+float: inline-start;
+@media (max-width:475px){
+        width: 385px;
+        height: 201px;
+    }
+`
