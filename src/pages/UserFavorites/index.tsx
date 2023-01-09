76639576257b7235d1465386600d65b77540f7e4
Victor Ledur <victorlledur@gmail.com>
2023-01-08 23:08:24 -0300

    feat: UserFavorites

---
+++ src/pages/UserFavorites/index.tsx
@@ -0,0 +1,17 @@
+import Header from "../../components/Header"
+
+
+function UserFavorites() {
+    return (
+        <div className="App">
+            <header>
+                <Header />
+            </header>
+            <main>
+
+            </main>
+        </div>
+    )
+}
+
+export default UserFavorites
---
+++ src/pages/UserFavorites/styles.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const SideBar = styled.div`
+    width: 33vw;
+    padding: 3vh 0px 0px 4vw;
+`
