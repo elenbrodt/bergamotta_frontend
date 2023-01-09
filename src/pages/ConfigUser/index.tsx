18dd79bfbf896409d591edc97f1696210511e7bc
Victor Ledur <victorlledur@gmail.com>
2023-01-08 23:07:53 -0300

    feat: ConfigUser

---
+++ src/pages/ConfigUser/index.tsx
@@ -0,0 +1,17 @@
+import Header from "../../components/Header"
+
+
+function ConfigUser (){
+    return(
+        <div className="App">
+            <header>
+                <Header />
+            </header>
+            <main>
+
+            </main>
+        </div>
+    )
+}
+
+export default ConfigUser
