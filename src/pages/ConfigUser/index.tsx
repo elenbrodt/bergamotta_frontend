4ce3676e663270cdb298b4163936f4ff3b743405
Victor Ledur <victorlledur@gmail.com>
2023-01-09 01:25:10 -0300

    update: ConfigUser

--- src/pages/ConfigUser/index.tsx
+++ src/pages/ConfigUser/index.tsx
@@ -1,5 +1,4 @@
 import Header from "../../components/Header"
-import Button from "../../components/Button"
 
 function ConfigUser (){
     return(
@@ -8,7 +7,6 @@ function ConfigUser (){
                 <Header />
             </header>
             <main>
-            <Button href="https://img.freepik.com/fotos-gratis/estilo-de-vida-beleza-e-moda-conceito-de-emocoes-de-pessoas-jovem-gerente-de-escritorio-feminino-asiatico-ceo-com-expressao-satisfeita-em-pe-sobre-um-fundo-branco-sorrindo-com-os-bracos-cruzados-sobre-o-peito_1258-59329.jpg?w=2000"></Button>
 
             </main>
         </div>
