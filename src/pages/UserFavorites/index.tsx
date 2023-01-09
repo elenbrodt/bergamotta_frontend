57a65d346ede25e8531262240f6af4ae43b3574f
Victor Ledur <victorlledur@gmail.com>
2023-01-09 03:58:33 -0300

    feat: UserFavorites

--- src/pages/UserFavorites/index.tsx
+++ src/pages/UserFavorites/index.tsx
@@ -1,5 +1,5 @@
 import Header from "../../components/Header"
-
+import { ImgRound, MainDiv, RoundImage, UserName, FavoritesDiv, FavoritesDivLeft, FavoritesDivRight, FavTitles } from "./styles"
 
 function UserFavorites() {
     return (
@@ -8,7 +8,20 @@ function UserFavorites() {
                 <Header />
             </header>
             <main>
-
+                <MainDiv>
+                    <RoundImage>
+                        <ImgRound src="https://img.freepik.com/fotos-gratis/estilo-de-vida-beleza-e-moda-conceito-de-emocoes-de-pessoas-jovem-gerente-de-escritorio-feminino-asiatico-ceo-com-expressao-satisfeita-em-pe-sobre-um-fundo-branco-sorrindo-com-os-bracos-cruzados-sobre-o-peito_1258-59329.jpg?w=2000" alt="test" />
+                    </RoundImage>
+                    <UserName>Nome</UserName>
+                    <FavoritesDiv>
+                        <FavoritesDivLeft> 
+                            <FavTitles>Seus Favoritos</FavTitles>
+                        </FavoritesDivLeft>
+                        <FavoritesDivRight> 
+                        <FavTitles>Ultimas avaliações</FavTitles>
+                        </FavoritesDivRight>
+                    </FavoritesDiv>
+                </MainDiv>
             </main>
         </div>
     )
--- src/pages/UserFavorites/styles.ts
+++ src/pages/UserFavorites/styles.ts
@@ -1,6 +1,47 @@
 import styled from 'styled-components';
+import { colors, fonts } from '../../styles/theme';
 
-export const SideBar = styled.div`
-    width: 33vw;
-    padding: 3vh 0px 0px 4vw;
+export const MainDiv = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+`
+export const RoundImage = styled.div`
+margin-top: 51px;
+width: 210px;
+height: 210px;
+border-radius: 100px;
+`
+export const ImgRound = styled.img`
+width: 210px;
+height: 210px;
+border-radius: 100px;
+`
+export const UserName = styled.h2`
+font-family: ${fonts.primary};
+font-weight: 700;
+font-size: 32px;
+line-height: 35px;
+color: ${colors.black};
+`
+export const FavoritesDiv = styled.div`
+display: flex;
+`
+export const FavoritesDivLeft = styled.div`
+display: flex;
+flex-wrap: wrap;
+`
+export const FavTitles = styled.h3`
+flex-grow: 2;
+width: 174px;
+height: 27px;
+font-family: ${fonts.secondary};
+font-weight: 700;
+font-size: 24px;
+line-height: 26px;
+color: ${colors.black};
+`
+export const FavoritesDivRight = styled.div`
+display: flex;
+flex-wrap: wrap;
 `
