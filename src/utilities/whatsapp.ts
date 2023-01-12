7afeeba7652357589884819fad2e76194fb5c008
Victor Ledur <victorlledur@gmail.com>
2023-01-12 00:28:08 -0300

    feat: whatsapp utility

---
+++ src/utilities/whatsapp.ts
@@ -0,0 +1,3 @@
+export function whatsApp(nome:string, whatsapp: string, mensagem:string){
+    window.open(`whatsapp://send?text=Nome: ${nome}%0ANúmero: ${whatsapp}%0AMensagem: ${mensagem}&phone=5551992710367`)
+}
