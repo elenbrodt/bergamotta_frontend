00388751e33799a6a5aa8ae53d7f58ca319759b4
Victor Ledur <victorlledur@gmail.com>
2023-01-13 20:09:56 -0300

    feat: blog_comment service

--- src/services/MainApi/blog_comment.tsx
+++ src/services/MainApi/blog_comment.tsx
@@ -1,5 +1,19 @@
 import baseApi from "./config"
 
-export function byIdUser(props: string){
+export function byIdUser(props: string) {
     return baseApi.get(`/user/${props}`)
+}
+
+export function createBlogComment(req: any, props: string) {
+    return baseApi.post(`/${props}/createblogcomment`,
+        {
+            comment: req.comment,
+            date: req.date,
+            recipe_id: req.recipe_id,
+            user_id: req.user_id
+        },
+        {
+            headers: {
+                'Authorization': `Bearer ${req.token}`              
+            }})
 }
