a3f1992402cb074d9bf405c2c9d70718ac8eb7bd
elenbrodt <elenbrodt@gmail.com>
2023-01-14 19:22:12 -0300

    Update header to change with user type

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,41 +4,14 @@ import Logo from "../../assets/image/logo.png";
 import { Ul, Li, DivHeader, Nav, ImgLogo, HamburguerMenu, NavLink } from "./style";
 import { useUser } from '../../store/modules/user';
 import { useEffect, useState } from "react";
-import { byIdOwner } from "../../services/MainApi/owner";
-import { byIdUser } from "../../services/MainApi/user_profile";
+import { useOwner } from "../../store/modules/owner";
 
 function Header() {
     const user = useUser();
-    const [userType, setUserType] = useState("");
-    
-    useEffect(()=>{
-        if(user.isLogged){
-            const idUser = user.findUser.id;
-            console.log(idUser)
-            const getData = async () => {
-                try {
-                    const responseOwner = await byIdOwner(idUser);
-                    const responseUser = await byIdUser(idUser);
-                    if(responseOwner.data.id){
-                        setUserType("owner")
-                    }
-                    console.log("aquiii agora",responseUser.data)
-                    if(responseUser.data.id){
-                        setUserType("owner")
-                    }
-                } catch (error: any) {
-                    if (error.code==='ERR_BAD_REQUEST'){
-                        setUserType("user")
-                    }
-                }
-            };
-            getData();
-        }
-    })
-    
-    return(
-        <header>
-            { (userType === "user") &&        
+    const owner = useOwner();
+
+    if(user.isLogged){
+        return (<header>
             <DivHeader>
                 <HamburguerMenu>
                     <MenuHamburguer />
@@ -57,10 +30,11 @@ function Header() {
                 </Nav>
                 <ButtonLogin text="Login" redirect="/perfil" />
             </DivHeader> 
-            }   
-            {
-                (userType === "owner") && 
-                <DivHeader>
+        </header>)
+    }
+    if(owner.isLogged){
+        return(<header>
+            <DivHeader>
                     <HamburguerMenu>
                         <MenuHamburguer />
                     </HamburguerMenu>
@@ -76,29 +50,30 @@ function Header() {
                         </Ul>
                     </Nav>
                     <ButtonLogin text="Login" redirect="/login" />
-                </DivHeader>
-            }
-            {
-                (!user.isLogged) && 
-                <DivHeader>
-                    <HamburguerMenu>
-                        <MenuHamburguer />
-                    </HamburguerMenu>
-                    <NavLink to="/">
-                        <ImgLogo src={Logo} alt="Logo Bergamotta" />
-                    </NavLink>
-                    <Nav>
-                        <Ul>
-                            <Li><NavLink to="/blog">Blog</NavLink></Li>
-                            <Li><NavLink to="/perfil">Perfil</NavLink></Li>
-                            <Li><NavLink to="/parceiro">Seja Um Parceiro</NavLink></Li>
-                            <Li><NavLink to="/sobrenos">Quem Somos</NavLink></Li>
-                            <Li><NavLink to="/cadastro">Cadastro</NavLink></Li>
-                        </Ul>
-                    </Nav>
-                    <ButtonLogin text="Login" redirect="/login" />
-                </DivHeader>
-            }
+            </DivHeader>
+        </header>)
+    }   
+    
+    return(
+        <header>
+            <DivHeader>
+                <HamburguerMenu>
+                    <MenuHamburguer />
+                </HamburguerMenu>
+                <NavLink to="/">
+                    <ImgLogo src={Logo} alt="Logo Bergamotta" />
+                </NavLink>
+                <Nav>
+                    <Ul>
+                        <Li><NavLink to="/blog">Blog</NavLink></Li>
+                        <Li><NavLink to="/perfil">Perfil</NavLink></Li>
+                        <Li><NavLink to="/parceiro">Seja Um Parceiro</NavLink></Li>
+                        <Li><NavLink to="/sobrenos">Quem Somos</NavLink></Li>
+                        <Li><NavLink to="/cadastro">Cadastro</NavLink></Li>
+                    </Ul>
+                </Nav>
+                <ButtonLogin text="Login" redirect="/login" />
+            </DivHeader>
         </header>
     )
 }
