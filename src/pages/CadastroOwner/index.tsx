9f4ee9d73903ec068c9b5c7db814121ba910f32b
Victor Lopes <victormoreiralopes@hotmail.com>
2023-01-15 11:41:27 -0300

    arrumando defeitos do style do cadastroOwner

--- src/pages/CadastroOwner/index.tsx
+++ src/pages/CadastroOwner/index.tsx
@@ -6,19 +6,22 @@ export default function CadastroOwner () {
     return(
         <>
     <Header/>
-  <div className='cadastro-owner'>
+  <form className='cadastro-owner'>
+  <div>
   <p>Nome</p>
   <input type="text" placeholder="      digite seu nome e sobrenome" className='configurando-input'/>
   <p>Email</p>
   <input type="email" placeholder="     example@gmail.com" className='configurando-input'/>
   <p>Senha</p>
   <input type="passwordt" placeholder="    digite sua senha" className='configurando-input'/>
+
   <p>Foto</p>
   <input type="urlr" placeholder="    Link da imagem" className='configurando-input'/>
+  </div>
   <div className='inputs-especificos'>
   <div>
   <p>CNPJ</p>
-  <input type="number" placeholder="    digite seu CNPJ" className='config-input-secondary'/>
+  <input type="number" placeholder="    digite seu CNPJ" className='config-input-secondary cnpj' />
   </div>
   <div>
   <p>Ocupação</p>
@@ -28,7 +31,7 @@ export default function CadastroOwner () {
   <div className='inputs-especificos-secondary'>
   <div>
   <p>Cidade</p>
-  <input type="text" placeholder="    digite sua cidade" className='config-input-tertiary'/>
+  <input type="text" placeholder="    digite sua cidade" className='config-input-tertiary cidade'/>
   </div>
   <div>
   <p>UF</p>
@@ -40,7 +43,7 @@ export default function CadastroOwner () {
   </div>
   </div>
   <button className="btn_cadastro_owner">Salvar alterações</button>
-  </div>
+  </form>
   </>
     )
 }
