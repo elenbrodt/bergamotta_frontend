6a2c563b1cbe2c108591690ff646fa798bf4de76
Victor Ledur <victorlledur@gmail.com>
2023-01-16 19:52:27 -0300

    feat: UserProfile

--- src/pages/UserProfile/index.tsx
+++ src/pages/UserProfile/index.tsx
@@ -8,13 +8,16 @@ import {
   FavoritesDivLeft,
   FavoritesDivRight,
   FavTitles,
+  PlacesDiv
 } from "./styles";
 import { useUser } from "../../store/modules/user";
 import { useEffect, useState } from "react";
 import { byIdUser } from "../../services/MainApi/user_profile";
-import { Link, Navigate, useNavigate } from "react-router-dom";
 import { Footer } from "../../components/Footer";
-//import { useNavigate } from "react-router-dom";
+import { userFavoritesById } from "../../services/MainApi/favorites";
+import CardPlace from "../../components/CardPlace";
+import { userratingById } from "../../services/MainApi/ratings";
+import ProfileRatingCard from "../../components/ProfileRatingCard";
 
 interface UserProfileProps {
   name: string;
@@ -43,6 +46,59 @@ function UserProfile() {
     }
   }, [setUserData, user]);
 
+  interface PlaceProps {
+    id: string
+    image_link: string;
+    name: string;
+    opening_hours: string;
+    average_ticket_price: number;
+  }
+  interface FavoritesProps {
+    id: string;
+    place: PlaceProps
+  }
+
+  const [placeList, setPlaceList] = useState<FavoritesProps[]>([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await userFavoritesById(user.findUser.id);
+        setPlaceList(response.data);
+      } catch (error) {
+        console.error(error)
+        alert("Deu algo errado no catch")
+      }
+    }
+    getData();
+  }, [setPlaceList, user])
+
+  interface PlacesProps {
+    id: string
+    image_link: string;
+    name: string;
+  }
+  interface RatingProps {
+    id: string;
+    comment: string
+    place: PlacesProps
+  }
+  
+  const [ratingList, setRatingList] = useState<RatingProps[]>([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await userratingById(user.findUser.id);
+        setRatingList(response.data);
+      } catch (error) {
+        console.error(error)
+        alert("Deu algo errado no catch")
+      }
+    }
+    getData();
+  }, [setRatingList, user])
+
   return (
     <div className='App'>
       <Header />
@@ -54,15 +110,29 @@ function UserProfile() {
           <UserName>{userData?.name}</UserName>
           <FavoritesDiv>
             <FavoritesDivLeft>
-              <FavTitles>Seus Favoritos</FavTitles>
+              <FavTitles>Seus favoritos</FavTitles>
+              <PlacesDiv>
+                {placeList.map((placeList, index) => index < 10 &&
+                  (
+                    <CardPlace id={placeList.place.id} image_link={placeList.place.image_link} name={placeList.place.name} opening_hours={placeList.place.opening_hours} average_ticket_price={placeList.place.average_ticket_price} />
+                  )
+                )}
+              </PlacesDiv>
             </FavoritesDivLeft>
             <FavoritesDivRight>
-              <FavTitles>Ultimas avaliações</FavTitles>
+              <FavTitles>Últimas avaliações</FavTitles>
+              <PlacesDiv>
+              {ratingList.map((ratingList, index) => index < 10 &&
+                  (
+                    <ProfileRatingCard id={ratingList.place.id} place={ratingList.place} comment={ratingList.comment} />
+                  )
+                )}
+              </PlacesDiv>
             </FavoritesDivRight>
           </FavoritesDiv>
         </MainDiv>
       </main>
-      <Footer />
+      <Footer/>
     </div>
   );
 }
