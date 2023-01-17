ba1869d124eb7fa82e222f8276ea7914241e5fdf
Victor Ledur <victorlledur@gmail.com>
2023-01-17 00:21:56 -0300

    update: new function

--- src/services/MainApi/ratings.tsx
+++ src/services/MainApi/ratings.tsx
@@ -26,3 +26,6 @@ export function serviceSpeed(props: string) {
 export function userratingById( props: string) {
   return baseApi.get(`/ratingbyuser/${props}`);
 }
+export function ratingByPlaceId( props: string) {
+  return baseApi.get(`/usercomment/${props}`);
+}
