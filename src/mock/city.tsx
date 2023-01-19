cd419213e86e2f637f71a26109a8f205abc4a92a
Elen Brodt <elenbrodt@gmail.com>
2023-01-19 18:03:49 -0300

    Update tags ids

--- src/mock/city.tsx
+++ src/mock/city.tsx
@@ -5,8 +5,8 @@ type City = {
   zoom: number;
 };
 export const CITY: City = {
-  title: 'São Paulo',
-  lat: -23.5406643,
-  lng: -46.6874576,
-  zoom: 13
+  title: "São Paulo",
+  lat: -23.575059711958566,
+  lng: -46.67954006345125,
+  zoom: 13,
 };
