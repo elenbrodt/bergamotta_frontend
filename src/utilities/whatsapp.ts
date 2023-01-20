ed0f23fa312520c366bf54909ff72df6f3f55dc6
Victor Ledur <victorlledur@gmail.com>
2023-01-20 14:46:14 -0300

    update: link whatsapp

--- src/utilities/whatsapp.ts
+++ src/utilities/whatsapp.ts
@@ -1,3 +1,3 @@
 export function whatsApp(nome:string, whatsapp: string, mensagem:string){
-    window.open(`whatsapp://send?text=Nome: ${nome}%0ANúmero: ${whatsapp}%0AMensagem: ${mensagem}&phone=555192681642`)
+    window.open(`https://wa.me/send?text=Nome: ${nome}%0ANúmero: ${whatsapp}%0AMensagem: ${mensagem}&phone=555192681642`)
 }
