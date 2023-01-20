388d854d7244dbf5e478d4fd9e221dd0d3223ebe
Elen Brodt <elenbrodt@gmail.com>
2023-01-20 13:20:54 -0300

    refactor: search api

--- src/services/MainApi/search.tsx
+++ src/services/MainApi/search.tsx
@@ -3,8 +3,3 @@ import baseApi from "./config";
 export function listPlace() {
   return baseApi.get("/places");
 }
-
-/* export function searchPlaces(query: string) {
-  return baseApi.get(`/search/?q=${query}`);
-}
- */
